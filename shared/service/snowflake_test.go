f99f1207cef00de9db1428a15caf803f47880566
alexradzin <alexander.radzin@firebolt.io>
2025-06-24 12:42:42 +0300

    added funciont NextBase62ID() and its test

--- shared/service/snowflake_test.go
+++ shared/service/snowflake_test.go
@@ -10,11 +10,24 @@ func TestGenerateId(t *testing.T) {
 	generator, err := NewDefaultGenerator()
 	assert.NoError(t, err)
 
-	id1 := generator.NextID()
-	assert.Len(t, EncodeBase62(id1), 10)
+	id1 := EncodeBase62(generator.NextID())
+	assert.Len(t, id1, 10)
 
-	id2 := generator.NextID()
-	assert.Len(t, EncodeBase62(id2), 10)
+	id2 := EncodeBase62(generator.NextID())
+	assert.Len(t, id2, 10)
+
+	assert.NotEqual(t, id1, id2)
+}
+
+func TestGenerateBase62Id(t *testing.T) {
+	generator, err := NewDefaultGenerator()
+	assert.NoError(t, err)
+
+	id1 := generator.NextBase62ID()
+	assert.Len(t, id1, 10)
+
+	id2 := generator.NextBase62ID()
+	assert.Len(t, id2, 10)
 
 	assert.NotEqual(t, id1, id2)
 }
