399a0568342d887a0996b5d37a24c75546f9ba19
Alexander Radzin <alexander_radzin@yahoo.com>
2025-07-15 12:05:49 +0300

    issue-72: added fields valid_from and valid_until to shorturl and cam… (#73)
    issue-72: added fields valid_from and valid_until to shorturl and campaign

--- shared/service/campaign/campaign.go
+++ shared/service/campaign/campaign.go
@@ -1,6 +1,8 @@
 package campaign
 
 import (
+	"time"
+
 	"github.com/lnk.by/shared/service"
 	"github.com/lnk.by/shared/utils"
 )
@@ -8,6 +10,8 @@ import (
 type Campaign struct {
 	ID             string       `json:"id"`
 	Name           string       `json:"name"`
+	ValidFrom      time.Time    `json:"validFrom"`
+	ValidUntil     time.Time    `json:"validUntil"`
 	OrganizationID string       `json:"organizationId"`
 	CustomerID     string       `json:"customerId"`
 	Status         utils.Status `json:"status"`
@@ -46,7 +50,7 @@ func (c *Campaign) Generate() {
 
 var (
 	CreateSQL   service.CreateSQL[*Campaign]   = "INSERT INTO campaign (id, name, organization_id, customer_id, status) VALUES ($1, $2, NULLIF($3, ''), NULLIF($4, ''), $5)"
-	RetrieveSQL service.RetrieveSQL[*Campaign] = "SELECT id, name, COALESCE(organization_id, ''), COALESCE(customer_id, ''), status FROM campaign WHERE id = $1 AND status='active'"
+	RetrieveSQL service.RetrieveSQL[*Campaign] = "SELECT id, name, COALESCE(organization_id, ''), COALESCE(customer_id, ''), status FROM campaign WHERE id = $1 AND status='active' AND now() BETWEEN valid_from AND valid_until"
 	UpdateSQL   service.UpdateSQL[*Campaign]   = "UPDATE campaign SET name = $2, organization_id = NULLIF($3, ''), customer_id = NULLIF($4, ''), status = $5 WHERE id = $1"
 	DeleteSQL   service.DeleteSQL[*Campaign]   = "DELETE FROM campaign WHERE id = $1"
 	ListSQL     service.ListSQL[*Campaign]     = "SELECT id, name, COALESCE(organization_id, ''), COALESCE(customer_id, ''), status FROM campaign WHERE status='active' OFFSET $1 LIMIT $2"
