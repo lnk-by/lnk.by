6939ffaf1efa9cc72597359b781bb75e9177615f
alexradzin <alexander.radzin@firebolt.io>
2025-08-12 14:11:35 +0300

    added  more logs for template list

--- aws/s3client/s3client.go
+++ aws/s3client/s3client.go
@@ -76,25 +76,35 @@ func Delete(ctx context.Context, path string) error {
 
 func List(ctx context.Context, prefix string, extension string) ([]string, error) {
 	var result []string
+	slog.Info("s3client.List 1")
 	input := &s3.ListObjectsV2Input{
 		Bucket: aws.String(s3Bucket),
 		Prefix: aws.String(prefix),
 	}
 
+	slog.Info("s3client.List 2")
 	paginator := s3.NewListObjectsV2Paginator(s3Client, input)
+	slog.Info("s3client.List 3")
 
 	for paginator.HasMorePages() {
+		slog.Info("s3client.List 4")
 		page, err := paginator.NextPage(ctx)
+		slog.Info("s3client.List 5")
 		if err != nil {
+			slog.Info("s3client.List 5.1", "error", err)
 			return nil, fmt.Errorf("listing S3 objects: s3://%s/%s/*.%s %w", s3Bucket, prefix, extension, err)
 		}
+		slog.Info("s3client.List 6")
 
 		for _, obj := range page.Contents {
+			slog.Info("s3client.List 7")
 			if strings.HasSuffix(*obj.Key, "."+extension) {
+				slog.Info("s3client.List 8")
 				result = append(result, *obj.Key)
 			}
 		}
 	}
+	slog.Info("s3client.List 9", "result", result)
 
 	return result, nil
 }
--- aws/template/list/main.go
+++ aws/template/list/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"log/slog"
 	"net/http"
 	"path"
 	"strings"
@@ -14,24 +15,35 @@ import (
 )
 
 func listLandingPageTemplate(ctx context.Context, request events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
+	slog.Info("listLandingPageTemplate 1")
 	templatePaths, err := s3client.List(ctx, landingpage.Path, "html")
+	slog.Info("listLandingPageTemplate 2")
 	if err != nil {
+		slog.Info("listLandingPageTemplate 2.1", "error", err)
 		return events.APIGatewayV2HTTPResponse{StatusCode: http.StatusInternalServerError, Body: toJSON(err), Headers: adapter.StandardHeaders}, nil
 	}
 
+	slog.Info("listLandingPageTemplate 3")
 	var templates []landingpage.LandingPageTemplate
 	for _, templatePath := range templatePaths {
+		slog.Info("listLandingPageTemplate 4", "templatePath", templatePath)
 		template, err := landingpage.RetrieveLandingPageTemplate(ctx, fileNameFromPath(templatePath))
+		slog.Info("listLandingPageTemplate 5")
 		if err != nil {
+			slog.Info("listLandingPageTemplate 5.1", "error", err)
 			return events.APIGatewayV2HTTPResponse{StatusCode: http.StatusInternalServerError, Body: toJSON(err), Headers: adapter.StandardHeaders}, nil
 		}
 		templates = append(templates, template)
 	}
+	slog.Info("listLandingPageTemplate 6")
 	bytes, err := json.Marshal(templates)
+	slog.Info("listLandingPageTemplate 7")
 	if err != nil {
+		slog.Info("listLandingPageTemplate 7.1", "error", err)
 		return events.APIGatewayV2HTTPResponse{StatusCode: http.StatusInternalServerError, Body: toJSON(err), Headers: adapter.StandardHeaders}, nil
 	}
 
+	slog.Info("listLandingPageTemplate 8")
 	return events.APIGatewayV2HTTPResponse{StatusCode: http.StatusOK, Body: string(bytes), Headers: adapter.StandardHeaders}, nil
 }
 
